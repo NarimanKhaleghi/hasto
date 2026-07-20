dedc13efe56c87191e34f6b9f72aa4b4d03644e2
NarimanKhaleghi <narimankhaleghi@gmail.com>
2026-07-20 15:22:46 +0330

    Cloudflare Pages deploy + fix turbopack root

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: { root: "./" },
   output: "export",
   images: { unoptimized: true },
   /* config options here */
